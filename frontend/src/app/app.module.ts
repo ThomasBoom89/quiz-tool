7f3fd99fc75daba9becefeef0a09a49b6996c7db
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-09-07 18:38:36 +0200

    Setup: Create angular application
    remove unnecessary test suite
    add tailwindcss

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './pages/user/user.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { OverviewComponent } from './pages/overview/overview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    AdminComponent,
+    OverviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
