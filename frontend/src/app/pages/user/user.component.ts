97f528d624ddc6eb53d93744a220d7c32cf23d77
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-09-12 23:23:27 +0200

    User: Add user login and redirect to room page

--- frontend/src/app/pages/user/user.component.ts
+++ frontend/src/app/pages/user/user.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+
+interface userFormGroup {
+  name: FormControl<string>;
+  roomId: FormControl<string>;
+}
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
+  public userForm: FormGroup<userFormGroup>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly userService: UserService) {
+    this.userForm = new FormGroup<userFormGroup>({
+      name: new FormControl<string>('', {nonNullable: true, validators: Validators.required}),
+      roomId: new FormControl<string>('', {nonNullable: true, validators: Validators.required}),
+    });
   }
 
+  public onSubmitUserForm() {
+    this.userService.register(this.userForm.controls.name.value, this.userForm.controls.roomId.value);
+  }
 }
