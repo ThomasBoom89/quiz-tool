a9603b192675739bd92fd2a5689f7c2794442995
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-09-12 23:10:15 +0200

    Router: Add routes to pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserComponent} from './pages/user/user.component';
+import {AdminComponent} from './pages/admin/admin.component';
+import {OverviewComponent} from './pages/overview/overview.component';
+import {UserRoomComponent} from './pages/user-room/user-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+  {
+    path: 'user/room/:roomId',
+    component: UserRoomComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+  {
+    path: 'overview',
+    component: OverviewComponent,
+  },
+  {path: '**', redirectTo: '/'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+export class AppRoutingModule {
+}
