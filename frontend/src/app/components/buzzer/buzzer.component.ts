a3f87b58aae355c03a9b2e06b74e3a20a1590b8c
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-09-12 23:00:47 +0200

    Buzzer: Add buzzer component

---
+++ frontend/src/app/components/buzzer/buzzer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buzzer',
+  templateUrl: './buzzer.component.html',
+})
+export class BuzzerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
