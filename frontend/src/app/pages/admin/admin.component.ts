e4d0153d67c28201cd324576e1cc37346f199d86
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-09-22 21:09:40 +0200

    Player: Add player and state to admin view

--- frontend/src/app/pages/admin/admin.component.ts
+++ frontend/src/app/pages/admin/admin.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Player} from '../../interfaces/player';
+import {PlayerState} from '../../enums/player-state';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+interface questionFormGroup {
+  question: FormControl<string>;
+}
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
 
-  constructor() { }
+  public players: Player[] = [
+    {id: 1, name: 'test', points: 4, state: PlayerState.none},
+    {id: 2, name: 'auch', points: 7, state: PlayerState.none},
+    {id: 2, name: 'noch', points: 7, state: PlayerState.none},
+    {id: 1, name: 'test', points: 4, state: PlayerState.none},
+    {id: 2, name: 'auch', points: 7, state: PlayerState.none},
+    {id: 2, name: 'diesisteinwirklichlangername', points: 7, state: PlayerState.none},
+    {id: 1, name: 'test', points: 4, state: PlayerState.none},
+    {id: 2, name: 'auch', points: 7, state: PlayerState.none},
+    {id: 2, name: 'noch', points: 7, state: PlayerState.active},
+    {id: 1, name: 'test', points: 4, state: PlayerState.none},
+    {id: 2, name: 'auch', points: 7, state: PlayerState.none},
+    {id: 2, name: 'noch', points: 7, state: PlayerState.none},
+    {id: 1, name: 'test', points: 4, state: PlayerState.none},
+    {id: 2, name: 'auch', points: 7, state: PlayerState.blocked},
+    {id: 2, name: 'noch', points: 7, state: PlayerState.none},
+    {id: 1, name: 'test', points: 4, state: PlayerState.none},
+    {id: 2, name: 'auch', points: 7, state: PlayerState.none},
+    {id: 2, name: 'noch', points: 7, state: PlayerState.none},
+  ];
 
-  ngOnInit(): void {
+  public questionForm: FormGroup<questionFormGroup>;
+
+  constructor() {
+    this.questionForm = new FormGroup<questionFormGroup>({
+      question: new FormControl<string>('', {nonNullable: true, validators: Validators.required}),
+    });
   }
 
+  public onSubmitQuestionForm() {
+  }
 }
