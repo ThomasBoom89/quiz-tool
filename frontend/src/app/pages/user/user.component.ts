103aacc0cb448baa9e4ca51eee2c50aa5794f649
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-10-01 17:29:13 +0200

    User: Add display of other player
    Also display own state and points

--- frontend/src/app/pages/user/user.component.ts
+++ frontend/src/app/pages/user/user.component.ts
@@ -1,20 +1,25 @@
 import {Component} from '@angular/core';
 import {UserService} from '../../services/user.service';
+import {QuizService} from '../../services/quiz.service';
+import {PlayerState} from '../../enums/player-state';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
 })
 export class UserComponent {
-  currentQuestion: string = 'Hier könnte deine Frage stehen!';
-
   constructor(
     public readonly userService: UserService,
+    public readonly quizService: QuizService,
   ) {
     this.userService.connect();
   }
 
   public buzzed() {
     this.userService.setBuzzed();
+  }
+
+  public isBuzzed(): boolean {
+    return this.userService.getState() === PlayerState.active;
   }
 }
